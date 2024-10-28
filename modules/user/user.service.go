5ea25e812115ded14310af8b2d4ac59fbe1a10b9
MazWas <waskito.kitoo@gmail.com>
2024-10-28 13:43:57 +0700

    fix : user.service.go sanitize DB return

--- modules/user/user.service.go
+++ modules/user/user.service.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"go-clean/db"
+	"go-clean/utils"
 )
 
 type NewUserService struct{}
@@ -10,7 +11,7 @@ func (s *NewUserService) GetUserByUsername(username string) (*ModelUser, error) 
 	var user ModelUser
 
 	if err := db.Data.Where("username = ?", username).First(&user).Error; err != nil {
-		return nil, err
+		return nil, utils.SanitizeDBError(err)
 	}
 
 	return &user, nil
@@ -19,13 +20,13 @@ func (s *NewUserService) GetUserByUsername(username string) (*ModelUser, error) 
 func (s *NewUserService) GetUserByEmail(email string) (*ModelUser, error) {
 	var user ModelUser
 	err := db.Data.Where("email = ?", email).First(&user).Error
-	return &user, err
+	return &user, utils.SanitizeDBError(err)
 }
 
 func (s *NewUserService) GetUserByID(id uint) (*ModelUser, error) {
 	var user ModelUser
 	err := db.Data.Find(&user, id).Error
-	return &user, err
+	return &user, utils.SanitizeDBError(err)
 }
 
 func (s *NewUserService) GetAllUsers(filters map[string]interface{}) ([]ModelUser, error) {
@@ -39,24 +40,24 @@ func (s *NewUserService) GetAllUsers(filters map[string]interface{}) ([]ModelUse
 	}
 
 	err := db.Data.Find(&users).Error
-	return users, err
+	return users, utils.SanitizeDBError(err)
 }
 
 func (s *NewUserService) CreateUser(user *ModelUser) error {
-	return db.Data.Create(user).Error
+	return utils.SanitizeDBError(db.Data.Create(user).Error)
 }
 
 func (s *NewUserService) UpdateUser(id uint, input RequestUpdateUser) error {
 	user, err := Service.GetUserByID(id)
 	if err != nil {
-		return err
+		return utils.SanitizeDBError(err)
 	}
-	return db.Data.Model(&user).Updates(input).Error
+	return utils.SanitizeDBError(db.Data.Model(&user).Updates(input).Error)
 }
 
 func (s *NewUserService) DeleteUser(id uint) error {
 	var user ModelUser
-	return db.Data.Delete(&user, id).Error
+	return utils.SanitizeDBError(db.Data.Delete(&user, id).Error)
 }
 
 var Service = &NewUserService{}
