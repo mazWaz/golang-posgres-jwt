51ed9ecd88e2c8e7a20cc552c10174dd9ddc3cab
MazWas <waskito.kitoo@gmail.com>
2024-10-28 13:44:24 +0700

    fix : user.routes.go securing middleware

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -12,28 +12,29 @@ type NewUserRoutes struct{}
 func (s *NewUserRoutes) Init(router *gin.Engine) {
 
 	userRoutes := router.Group("/api/user")
-	//userRoutes.Use(middlewares.AuthMiddleware())
+	userRoutes.Use(middlewares.AuthMiddleware())
 	{
 		userRoutes.GET("/profile",
 			middlewares.Role(user.SUPERADMIN, user.ADMIN, user.USER),
 			user.Controller.GetProfile)
 
 		userRoutes.GET("/",
-			//middlewares.Role(user.SUPERADMIN),
+			middlewares.Role(user.SUPERADMIN),
+			middlewares.ValidationMiddleware(&user.RequestQueryUser{}, nil),
 			user.Controller.GetUsers)
 
 		userRoutes.GET("/:id",
-			//middlewares.Role(user.SUPERADMIN),
-			//middlewares.ValidationMiddleware(user.RequestQueryUser{}, nil),
+			middlewares.Role(user.SUPERADMIN),
 			user.Controller.GetUser)
 
 		userRoutes.POST("/",
-			//middlewares.Role(user.SUPERADMIN),
+			middlewares.Role(user.SUPERADMIN),
 			middlewares.ValidationMiddleware(nil, &user.RequestCreateUser{}),
 			user.Controller.CreateUser)
 
 		userRoutes.PATCH("/:id",
 			middlewares.Role(user.SUPERADMIN),
+			middlewares.ValidationMiddleware(nil, &user.RequestUpdateUser{}),
 			user.Controller.UpdateUSer)
 
 		userRoutes.DELETE("/:id",
